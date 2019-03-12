6ed4d5545b22c61062e909619eb29f6dacdf0f06
Dankken <daniel.jakob.nyberg@gmail.com>
2019-03-12 15:02:11 +0100

    something

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public int speed;
+
+    private Rigidbody2D rb2d;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+      
+        Vector2 movement = new Vector2(1,0);
+
+        rb2d.AddForce(movement * speed);
     }
 }
