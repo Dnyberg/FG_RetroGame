c1803226baa0ee0bec7bc771fce4623664d9686d
Dankken <daniel.jakob.nyberg@gmail.com>
2019-03-18 13:41:29 +0100

    Level platform generator WIP

---
+++ Assets/Scripts/PlatformGenrator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenrator : MonoBehaviour
+{
+
+    public GameObject thePlate;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    public ObjectPooler theObjectPool;
+
+    private float platformWidth;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platformWidth = thePlate.GetComponent<BoxCollider2D>().size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < generationPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.y);
+            //Instantiate(thePlate, transform.position, transform.rotation);
+
+            GameObject newPlatform = theObjectPool.GetPooledObject();
+            newPlatform.transform.position = transform.position;
+            newPlatform.transform.rotation = transform.rotation;
+            newPlatform.SetActive(true);
+        }
+
+    }
+}
